8b98889b25b41f9d281293adb99271b700802c18
SamMrach <sammerche84@gmail.com>
2024-09-30 11:30:04 +0100

    update header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function Header() {
-    const lastExportDate="01/06/2024";
+    const lastExportDate="20/09/2024";
   return (
     <div className='mb-6'>
         <h3 className='text-gray-400'>Production Functionality Matrix</h3>
